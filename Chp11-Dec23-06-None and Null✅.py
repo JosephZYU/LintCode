de9e36cc94a6c9c0c320c61886db3c6e6415c69a
JosephYu <josephyu.careers@outlook.com>
2020-12-28 15:30:43 +0800

    Add 06

---
+++ Chp11-Dec23-06-None and Null✅.py
@@ -0,0 +1,11 @@
+"""
+
+EVERYTHING IN PYTHON IS AN OBJECT!
+
+None v.s. Null
+
+1. None is used to define no value at all.
+2. None is NOT the same as 0, false, or an empty string
+3. None is a Singleton, object of class NoneType
+
+"""
