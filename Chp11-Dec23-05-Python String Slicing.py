46d183502013fc1f590b8863ecbb12348454f614
JosephYu <josephyu.careers@outlook.com>
2020-12-28 14:51:16 +0800

    Add 05

---
+++ Chp11-Dec23-05-Python String Slicing.py
@@ -0,0 +1,14 @@
+# Python slicing
+
+# 🎯 how to convert a, b, c with quotes auaomaticallly!
+
+list_s = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+print(len(list_s))
+
+print(list_s[1:4:1])
+print(list_s[0:6:2])
+print(list_s[0::2])
+print(list_s[::2])
+print(list_s[2:-2:2])
+# print(list_s[-6:-2:1])
